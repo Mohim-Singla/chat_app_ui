549fcc7157904373c19024ef8dea1346bc8a0d1d
Mohim-Singla <mohimsingla@gmail.com>
2024-12-14 17:31:36 +0530

    type(feat): Validate user token on home page

--- src/components/dashboard/home.js
+++ src/components/dashboard/home.js
@@ -1,9 +1,44 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom"; // Use this if using React Router
 import SidePanel from "./sidePanel/sidePanel";
+import { serviceConfig } from "../../config/config";
 
 function Home() {
-  return (
-    <SidePanel />
-  );
-};
+  const navigate = useNavigate(); // For navigation
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const user = localStorage.getItem("user");
+      const token = user ? JSON.parse(user).token : null;
+
+      try {
+        const response = await fetch(
+          `${serviceConfig.AUTH_SERVER.HOST}${serviceConfig.AUTH_SERVER.ENDPOINTS.VALIDATE_TOKEN}`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        if (response.status !== 200) {
+          localStorage.clear();
+          navigate("/login");
+          return;
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        localStorage.clear();
+        navigate("/login");
+      }
+    };
+
+    fetchData();
+  }, [navigate]);
+
+  return <SidePanel />;
+}
 
 export default Home;
--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,8 @@ export const serviceConfig = {
     HOST: process.env.REACT_APP_AUTH_SERVER_HOST,
     ENDPOINTS: {
       LOGIN: process.env.REACT_APP_AUTH_SERVER_LOGIN,
-      SIGNUP: process.env.REACT_APP_AUTH_SERVER_SIGNUP
+      SIGNUP: process.env.REACT_APP_AUTH_SERVER_SIGNUP,
+      VALIDATE_TOKEN: process.env.REACT_APP_VALIDATE_TOKEN,
     },
   }
 };
